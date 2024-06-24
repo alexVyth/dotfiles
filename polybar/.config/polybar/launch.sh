da035281515630393560c2176adbbaadd5bf8a11
Alexandros Vythoulkas <alvythoulkas@gmail.com>
2024-06-24 16:23:46 +0300

    Remove X11 configs

--- polybar/.config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Terminate already running bar instances
-polybar-msg cmd quit
-
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
-
-echo "Polybar launched..."
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,5 +1,3 @@
-export BATTERY="$(ls /sys/class/power_supply/ | grep -E '^BAT[0-9]+')"
-
 if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
   exec sway
 fi
